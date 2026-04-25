efd09a772871e0b92ee5a5b8ed76383745e5c372
MarcoGanassin <marcoganassin96@gmail.com>
2026-04-25 15:53:57 +0200

    feat(db): Aurora connection module with connection reuse

---
+++ src/db.py
@@ -0,0 +1,21 @@
+import os
+import psycopg2
+import psycopg2.extras
+from src.fields import EnvVar
+
+_connection = None
+
+
+def get_connection():
+    global _connection
+    if _connection is None or _connection.closed:
+        _connection = psycopg2.connect(
+            host=os.environ[EnvVar.DB_HOST],
+            port=int(os.environ.get(EnvVar.DB_PORT, "5432")),
+            dbname=os.environ[EnvVar.DB_NAME],
+            user=os.environ[EnvVar.DB_USER],
+            password=os.environ[EnvVar.DB_PASSWORD],
+            connect_timeout=5,
+            cursor_factory=psycopg2.extras.RealDictCursor,
+        )
+    return _connection
