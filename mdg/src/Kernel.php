e396f6c7814b50fa0bac86ee11df7b8b142305af
MarcoGanassin <marcoganassin96@gmail.com>
2026-06-08 11:48:27 +0200

    Revert "fix(mdg): tag Nelmio controllers via high-priority compiler pass"
    This reverts commit c2777f131720e47a8c52e250825c8d23bdb58491.

--- mdg/src/Kernel.php
+++ mdg/src/Kernel.php
@@ -2,28 +2,9 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
-use Symfony\Component\DependencyInjection\Compiler\PassConfig;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    protected function build(ContainerBuilder $container): void
-    {
-        // Priority 10 ensures this runs before RegisterControllerArgumentLocatorsPass (priority 0),
-        // which builds the allowControllers whitelist from controller.service_arguments tags.
-        $container->addCompilerPass(new class implements CompilerPassInterface {
-            public function process(ContainerBuilder $container): void
-            {
-                foreach (['nelmio_api_doc.controller.swagger_ui', 'nelmio_api_doc.controller.swagger_json'] as $id) {
-                    if ($container->hasDefinition($id)) {
-                        $container->getDefinition($id)->addTag('controller.service_arguments');
-                    }
-                }
-            }
-        }, PassConfig::TYPE_BEFORE_OPTIMIZATION, 10);
-    }
 }
